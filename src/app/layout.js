8d222bbb42fc16a12dd06afb0f1b6409065b2909
Nirav <niravpatelp129@gmail.com>
2024-08-08 19:59:10 -0400

    * feat(layout.js): updated title to 'TimeBlock' and added metadata in Head

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,10 +1,11 @@
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Todo List with Pomodoro Tracking',
+  title: 'TimeBlock',
   description:
     'An over-optimized todo list app with integrated Pomodoro tracking, created by an optimization wizard.',
 };
@@ -12,6 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name='description' content={metadata.description} />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   );
