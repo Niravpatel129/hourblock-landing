a22eb991b5ca7031ac147ce7dd4ce5601c55eff8
Nirav <niravpatelp129@gmail.com>
2024-08-16 17:54:47 -0400

    * fix(utils): updated download link

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
 export const DOWNLOAD_LINK =
-  'https://github.com/Niravpatel129/timebank/releases/download/v1.0.31/HourBlock-1.0.31-arm64.dmg';
+  'https://github.com/Niravpatel129/timebank/releases/download/v1.0.33/HourBlock-1.0.33-arm64.dmg';
